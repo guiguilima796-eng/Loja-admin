3c4e16ce62594cf2cdf67867e4f0fa42117199ac
Gui <guiguilima796@gmail.com>
2025-11-06 16:56:35 -0300

    add models

--- LOJA/produtos/models.py
+++ LOJA/produtos/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(
+        "Nome do Produto",
+        max_length=100,
+        help_text="Informe o nome do produto"
+    )
+    preco = models.CharField(
+        "Preço de Venda",
+        max_digits=8,
+        decimal_place=2,
+        help_text="Informe o Preço Unitario em Reais do Produto"
+        )
+    estoque = models.PositiveIntegerField(
+        "quantidade_em_estoque",
+        default=0,
+        help_text="Quantidade de produtos em estoque"
+    )
+    ativo = models.BooleanField(
+        "Disponivel para Venda",
+        default= True,
+        help_text="Marque-se o produto que está ativo"
+    )
